38c6463c51dfa4ec58e8a024947575807d8cebcf
saketsingh0078 <saketkrsingh0078@gmail.com>
2024-07-21 22:34:35 +0530

    new

---
+++ Frontend/src/components/Accordian.jsx
@@ -0,0 +1,17 @@
+import React, { useState } from "react";
+import { AccordianData } from "./AccordianData";
+
+export const Accordian = () => {
+  return (
+    <div className="">
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+      <AccordianData />
+    </div>
+  );
+};
